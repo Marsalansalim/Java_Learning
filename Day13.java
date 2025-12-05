e73035d0e8a1b30f468693a5aef7b6c036d8ac16
Mohammad Arsalan <marsalansalim2002@gmail.com>
2025-12-05 18:13:01 +0530

    Understand the basic concepts of Collection Framework with TreeMap.

---
+++ Day13.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DemoTreeMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<Integer, String> cities;
+		cities = new TreeMap<Integer,String>();
+		
+		cities.put(22,"Mumbai");
+		cities.put(11,"Delhi");
+		cities.put(33, "Pune");
+		
+		System.out.println(cities);
+		System.out.println(cities.size());
+		System.out.println(cities.get(22));
+		System.out.println(cities.containsKey(11));
+		System.out.println(cities.containsValue("Pune"));
+		
+		//duplicate key not allowed
+		cities.put(11,"Banglore");
+		System.out.println(cities);
+		
+		//duplicate value  allowed
+		cities.put(20,"Mumbai");
+		System.out.println(cities);
+		
+		cities.remove(33);
+		System.out.println(cities);
+		
+		cities.clear();
+
+	}
+
+}
